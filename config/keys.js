02dc7cc39914614cc75f535ecbd0fb7770b685a5
Weilyu Wang <weilyuwang@gmail.com>
2019-02-20 22:53:00 -0500

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://Weilyu0:940408@cluster0-shard-00-00-tkvl7.mongodb.net:27017,cluster0-shard-00-01-tkvl7.mongodb.net:27017,cluster0-shard-00-02-tkvl7.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+};
