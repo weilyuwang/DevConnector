d65cea2797f93ee4e8a286ada15038ad7c2958a6
Weilyu Wang <weilyuwang@gmail.com>
2019-02-23 15:08:08 -0500

    profile

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,10 +4,13 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 
+import { clearCurrentProfile } from "../../actions/profileActions";
+
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
     this.props.logoutUser();
+    this.props.clearCurrentProfile();
   }
   render() {
     const { isAuthenticated, user } = this.props.auth;
@@ -91,5 +94,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, clearCurrentProfile }
 )(Navbar);
